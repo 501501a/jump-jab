177cf4e40a72588c2c3816cbe5a8c90bc0e87009
501501a <2501agency@gmail.com>
2026-03-04 01:22:40 +0900

    2-03 JPA로 데이터베이스 사용하기

---
+++ src/main/java/com/back/JumpJsbApplication.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpJsbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumpJsbApplication.class, args);
+    }
+
+}
